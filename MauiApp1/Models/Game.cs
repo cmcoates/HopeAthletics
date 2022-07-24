dc8e8ad69c6c2eacb9ab78ec99b04c709bcb073b
00cmcoates <cameron.coates@hope.edu>
2022-07-24 13:54:23 -0400

    Retrieved schedules for men's sports

---
+++ MauiApp1/Models/Game.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1.Models
+{
+    public class Game
+    {
+        public string Date { get; set; }
+        public string Location { get; set; }
+        public string Opponent { get; set; }
+        public string Result { get; set; }
+    }
+}
--- MauiApp1/Views/AthleticsMen.xaml.cs
+++ MauiApp1/Views/AthleticsMen.xaml.cs
@@ -10,8 +10,9 @@ public partial class AthleticsMen : ContentPage
     public ObservableCollection<String> Sports { get; set; } = new();
 
     public ObservableCollection<Athlete> Athletes { get; set; } = new();
-    
 
+    public ObservableCollection<Game> Games { get; set; } = new();
+    public String CurrentMonth { get; set; }
 
     public AthleticsMen()
 	{
@@ -28,13 +29,31 @@ public partial class AthleticsMen : ContentPage
     private void OnPickerSelectedIndexChanged(object sender, EventArgs e)
     {
         Athletes.Clear();
+        Games.Clear();
         var picker = (Picker)sender;
         int selectedIndex = picker.SelectedIndex;
 
         if(selectedIndex != -1)
         {
             GetRoster(selectedIndex);
+            GetSchedule(selectedIndex);
         }
+        RosterBtn.IsVisible = true;
+        ScheduleBtn.IsVisible = true;
+        RosterList.IsVisible = false;
+        ScheduleList.IsVisible = false;
+    }
+
+    private void OnRosterClicked(object sender, EventArgs e)
+    {
+        ScheduleList.IsVisible = false;
+        RosterList.IsVisible = true;
+    }
+
+    private void OnScheduleClicked(object sender, EventArgs e)
+    {
+        RosterList.IsVisible = false;
+        ScheduleList.IsVisible = true;
     }
 
     private void GetRoster(int sport)
@@ -54,7 +73,8 @@ public partial class AthleticsMen : ContentPage
         var athls = docB.DocumentNode.SelectNodes("//div[@class='roster']//table//tbody//tr");
         if (athls is not null)
         {
-            NotFound.Text = "";
+            RosterNotFound.Text = "";
+            ScheduleNotFound.Text = "";
             foreach (var a in athls)
             {
                 String htmlAthleteInfo = a.InnerHtml;
@@ -98,7 +118,86 @@ public partial class AthleticsMen : ContentPage
         }
         else
         {
-            NotFound.Text = "Roster not available";
+            RosterNotFound.Text = "Roster not available";
+        }
+    }
+
+    private void GetSchedule(int sport)
+    {
+        HtmlWeb web = new();
+        HtmlDocument docA = web.Load("https://athletics.hope.edu/navbar-men-sports");
+        var mensSchedule = docA.DocumentNode.SelectNodes("//li[@class='has-nav']//ul[@class='clearfix']//li[1]");
+        mensSchedule.RemoveAt(10);
+        var msport = mensSchedule[sport];
+        String path = msport.InnerHtml;
+        string pattern = @"/sports/[\w/-]*";
+        Regex rg = new(pattern);
+        Match match = rg.Match(path);
+        path = match.Value;
+
+        HtmlDocument docB = web.Load("https://athletics.hope.edu" + path);
+        var sched = docB.DocumentNode.SelectNodes("//div[@class='schedule-content clearfix']//table//tbody//tr");
+        if (sched is not null)
+        {
+            ScheduleNotFound.Text = "";
+            foreach (var a in sched)
+            {
+                String htmlScheduleInfo = a.InnerHtml;
+                htmlScheduleInfo = htmlScheduleInfo.Replace("\t", "");
+                htmlScheduleInfo = htmlScheduleInfo.Replace("\n", "");
+                htmlScheduleInfo = htmlScheduleInfo.Replace(" ", "");
+
+                if (htmlScheduleInfo.Contains("tdcolspan=\"6\""))
+                {
+                    string monthPattern = "(?<=<tdcolspan=\"6\">)[\\w]*";
+                    Regex monthReg = new(monthPattern);
+                    match = monthReg.Match(htmlScheduleInfo);
+                    if(!match.Value.Equals(""))
+                    {
+                        CurrentMonth = match.Value;
+                    }
+                }
+                else
+                {
+                    string schedulePattern = "(?<=<tdclass=\"e_date\">)[\\w.]*";
+                    Regex dateReg = new(schedulePattern);
+                    match = dateReg.Match(htmlScheduleInfo);
+                    String date = match.Value;
+                    date = CurrentMonth + " " + date;
+
+                    schedulePattern = "(?<=<spanclass=\"va\">)[\\w]*";
+                    Regex locationReg = new(schedulePattern);
+                    match = locationReg.Match(htmlScheduleInfo);
+                    String location = match.Value;
+
+                    schedulePattern = "(?<=<spanclass=\"team-name\">)[\\w]*";
+                    Regex opponentReg = new(schedulePattern);
+                    match = opponentReg.Match(htmlScheduleInfo);
+                    String opponent = match.Value;
+
+                    schedulePattern = "(?<=<tdclass=\"e_result\">)[\\w,-]*";
+                    Regex resultReg = new(schedulePattern);
+                    match = resultReg.Match(htmlScheduleInfo);
+                    String result = match.Value;
+                    if (result.Equals(""))
+                    {
+                        result = "TBD";
+                    }
+
+                    Game game = new()
+                    {
+                        Date = date,
+                        Location = location,
+                        Opponent = opponent,
+                        Result = result
+                    };
+                    Games.Add(game);
+                }
+            }
+        }
+        else
+        {
+            ScheduleNotFound.Text = "Schedule not available";
         }
     }
 
