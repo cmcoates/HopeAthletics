bbb54778d9906f6c8d88fb9f5d36d0bf7a169554
00cmcoates <cameron.coates@hope.edu>
2022-07-26 16:57:29 -0400

    Added intramurals. Got standings listed, working on schedule

---
+++ MauiApp1/Models/Intramural.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1.Models
+{
+    public class Intramural
+    {
+        public string Sport { get; set; }
+        public string Season { get; set; }
+        public string Link { get; set; }
+        public ObservableCollection<Standing> Standings { get; set; }
+        public Schedule Schedule { get; set; }
+    }
+}
---
+++ MauiApp1/Models/Schedule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1.Models
+{
+    public class Schedule
+    {
+        public string Date { get; set; }
+        public string Location { get; set; }
+        public string HomeTeam { get; set; }
+        public string AwayTeam { get; set; }
+        public string Score { get; set; }
+        public string Time { get; set; }
+    }
+}
